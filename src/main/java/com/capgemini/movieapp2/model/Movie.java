8da02ee21a4690e48069ae7e5167dd56d10fffd5
tijsstraatman <tijsstraatman@gmail.com>
2017-10-17 09:50:33 +0200

    Model toegevoegd en correcte mappen

---
+++ src/main/java/com/capgemini/movieapp2/model/Movie.java
@@ -0,0 +1,44 @@
+package com.capgemini.movieapp2.model;
+
+public class Movie {
+
+    private String title;
+    private String rating;
+    private Boolean watched;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String rating, Boolean watched) {
+        this.title = title;
+        this.rating = rating;
+        this.watched = watched;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Boolean getWatched() {
+        return watched;
+    }
+
+    public void setWatched(Boolean watched) {
+        this.watched = watched;
+    }
+
+
+}
+
