432b67dab1b9cd4d843d2e6fc19e4c100505b4ac
tijsstraatman <tijsstraatman@gmail.com>
2017-10-17 12:47:03 +0200

    AJAX

--- src/main/resources/public/js/movie.js
+++ src/main/resources/public/js/movie.js
@@ -1,19 +1,49 @@
 $(document).ready(function() {
-    var t = $('#example').DataTable();
+    var t = $('#dataTable').DataTable();
     var counter = 1;
 
-    $('#addRow').on( 'click', function () {
-        t.row.add( [
-            counter +'.1',
-            counter +'.2',
-            counter +'.3',
-            counter +'.4',
-            counter +'.5'
-        ] ).draw( false );
-
-        counter++;
+    $('#addButton').on( 'click', function () {
+       createMovie();
     } );
 
     // Automatically add a first row of data
     $('#addRow').click();
 } );
+
+function createMovie() {
+    var obj = {
+        firstName: $("#addFirstName").val(),
+        lastName: $("#addLastName").val(),
+        address: $("#addAddress").val(),
+        postalCode: $("#addPostalCode").val(),
+        city: $("#addCity").val(),
+        country: $("#addCountry").val(),
+        phoneNumber: $("#addPhoneNumber").val(),
+        email: $("#addEmail").val()
+    };
+    makeAjaxRequest("POST", "/addGuest", obj, function(guest) {
+        if (guest) {
+            console.log(true);
+        console.log(guest);
+        $("#dataTable tbody").append("<tr><td>" + guest.id +
+            "</td><td>" + guest.firstName +
+            "</td><td>" + guest.lastName +
+            "</td><td>" + guest.address +
+            "</td><td>" + guest.postalCode +
+            "</td><td>" + guest.city +
+            "</td><td>" + guest.country +
+            "</td><td>" + guest.phoneNumber +
+            "</td><td>" + guest.email +
+            "</td><td>" + "<a href=\"javascript:del(" + guest.id + ")\" class=\"btn btn-danger\">Delete</a>" +
+            "</td></tr>");
+        refreshTable();
+        } else {
+            console.log(false);
+             alert("some of your input is not correct, please verify your input");
+        }
+
+function refreshTable() {
+    table.clear();
+    populateTable();
+    table.draw();
+}
