f6f53fc0423f59a6e54f6f5c41c866bcad4f907b
tijsstraatman <tijsstraatman@gmail.com>
2017-10-17 09:45:14 +0200

    Initial commit

---
+++ src/main/java/com/capgemini/movieapp2/Movieapp2Application.java
@@ -0,0 +1,12 @@
+package com.capgemini.movieapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movieapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movieapp2Application.class, args);
+	}
+}
